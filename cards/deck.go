6671ff75f1d8eace07d56729c0d1d16a4013ff71
andrea c <ancheetz@me.com>
2021-03-07 18:06:09 -0500

    cards program

---
+++ cards/deck.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+//create a new type of 'deck, made of a slice of strings
+
+type deck []string
+
+//create a new function (method) that is called on type deck
+func newDeck() deck {
+	//create and lists a deck of cards
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+
+	}
+	return cards
+}
+
+//needs to list the receiver/ variable to print out, d --> function print is only callable on receiver "deck"
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
+
+//create a deal function on type deck
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
+//convert a slice of deck into a string. this function only applies to d - the specific instance of type deck
+func (d deck) toString() string {
+	//strings is a package with function Join, which takes 2 arguments; we are joining the multiple strings w a comma to build one string
+	return strings.Join([]string(d), ",")
+}
+
+//create a func to save a list of cards to a file on my local machine
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+//create new deck from a file on harddrive
+
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		//if an error occurs, we can either log the error and return a call to newDeck()
+		//or log the error and completely exit the program
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+
+	//converting the bs into a string
+	s := strings.Split(string(bs), ",")
+	return deck(s)
+}
