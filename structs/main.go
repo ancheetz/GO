bdd2ac2011b73282f279674e4e4b9668f55b8951
andrea c <ancheetz@me.com>
2021-03-12 18:06:56 -0500

    structs

---
+++ structs/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
+type person struct {
+	firstName string
+	lastName  string
+	contactInfo
+}
+
+func main() {
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contactInfo: contactInfo{
+			email:   "jim@gmail.com",
+			zipCode: 123435,
+		},
+	}
+
+	jim.print()
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
+}
